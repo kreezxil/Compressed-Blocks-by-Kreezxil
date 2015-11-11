96a4c64b6a2465512952cbbc1f86861a89e2d359
The_Fireplace <the.f1repl4ce@gmail.com>
2015-11-10 19:59:33 -0600

    Work toward flint blockstate

--- src/main/java/com/kreezxil/compressedblocks/ModBlocks.java
+++ src/main/java/com/kreezxil/compressedblocks/ModBlocks.java
@@ -1,8 +1,5 @@
 package com.kreezxil.compressedblocks;
 
-import net.minecraft.block.Block;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
 import com.kreezxil.compressedblocks.blocks.ActivatedCarbonite;
 import com.kreezxil.compressedblocks.blocks.CompressedCoalBlock;
 import com.kreezxil.compressedblocks.blocks.CompressedCobblestone;
@@ -14,46 +11,42 @@ import com.kreezxil.compressedblocks.blocks.CompressedTNT;
 import com.kreezxil.compressedblocks.blocks.DoubleCompressedCoalBlock;
 import com.kreezxil.compressedblocks.blocks.DoubleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.DoubleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.DoubleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.DoubleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.DoubleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.OctupleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.OctupleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.OctupleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.OctupleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.OctupleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedCoalBlock;
 import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.QuadrupleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.QuintupleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.QuintupleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.QuintupleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.QuintupleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.QuintupleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.SeptupleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.SeptupleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.SeptupleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.SeptupleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.SeptupleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.SextupleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.SextupleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.SextupleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.SextupleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.SextupleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.TripleCompressedCoalBlock;
 import com.kreezxil.compressedblocks.blocks.TripleCompressedCobblestone;
 import com.kreezxil.compressedblocks.blocks.TripleCompressedDirt;
-import com.kreezxil.compressedblocks.blocks.TripleCompressedFlint;
 import com.kreezxil.compressedblocks.blocks.TripleCompressedGravel;
 import com.kreezxil.compressedblocks.blocks.TripleCompressedSand;
 import com.kreezxil.compressedblocks.blocks.coalAndGravelMix;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public final class ModBlocks {
-	
-    public static Block coalAndGravelMix;
+
+	public static Block coalAndGravelMix;
 	public static Block ActivatedCarbonite;
 	public static Block CompressedCobblestone;
 	public static Block CompressedCoalBlock;
@@ -100,67 +93,60 @@ public final class ModBlocks {
 	public static Block TripleCompressedFlint;
 	public static Block TripleCompressedGravel;
 	public static Block TripleCompressedSand;
-	
-	
+
+
 	public static void reg(Block block, String UnlocalizedName) {
 		GameRegistry.registerBlock(block, UnlocalizedName);
 	}
-	
+
 	public static void regFuel() {
 		GameRegistry.registerFuelHandler(new createFuel());
 	}
-	
-	public static void createBlocks() {
 
-	    reg(coalAndGravelMix = new coalAndGravelMix("coalAndGravelMix"),"coalAndGravelMix");
+	public static void createBlocks() {
+		reg(CompressedFlint = new CompressedFlint(),"CompressedFlint");
+
+		reg(coalAndGravelMix = new coalAndGravelMix("coalAndGravelMix"),"coalAndGravelMix");
 		reg(ActivatedCarbonite = new ActivatedCarbonite("ActivatedCarbonite"),"ActivatedCarbonite");
 		reg(CompressedCobblestone = new CompressedCobblestone("CompressedCobblestone"),"CompressedCobblestone");
 		reg(CompressedCoalBlock = new CompressedCoalBlock("CompressedCoalBlock"),"CompressedCoalBlock");
 		reg(CompressedDirt = new CompressedDirt("CompressedDirt"),"CompressedDirt");
-		reg(CompressedFlint = new CompressedFlint("CompressedFlint"),"CompressedFlint");
 		reg(CompressedGravel = new CompressedGravel("CompressedGravel"),"CompressedGravel");
 		reg(CompressedSand = new CompressedSand("CompressedSand"),"CompressedSand");
 		reg(CompressedTNT = new CompressedTNT("CompressedTNT"),"CompressedTNT");
 		reg(DoubleCompressedCoalBlock = new DoubleCompressedCoalBlock("DoubleCompressedCoalBlock"),"DoubleCompressedCoalBlock");
 		reg(DoubleCompressedCobblestone = new DoubleCompressedCobblestone("DoubleCompressedCobblestone"),"DoubleCompressedCobblestone");
 		reg(DoubleCompressedDirt = new DoubleCompressedDirt("DoubleCompressedDirt"),"DoubleCompressedDirt");
-		reg(DoubleCompressedFlint = new DoubleCompressedFlint("DoubleCompressedFlint"),"DoubleCompressedFlint");
 		reg(DoubleCompressedGravel = new DoubleCompressedGravel("DoubleCompressedGravel"),"DoubleCompressedGravel");
 		reg(DoubleCompressedSand = new DoubleCompressedSand("DoubleCompressedSand"),"DoubleCompressedSand");
 		reg(OctupleCompressedCobblestone = new OctupleCompressedCobblestone("OctupleCompressedCobblestone"),"OctupleCompressedCobblestone");
 		reg(OctupleCompressedDirt = new OctupleCompressedDirt("OctupleCompressedDirt"),"OctupleCompressedDirt");
-		reg(OctupleCompressedFlint = new OctupleCompressedFlint("OctupleCompressedFlint"),"OctupleCompressedFlint");
 		reg(OctupleCompressedGravel = new OctupleCompressedGravel("OctupleCompressedGravel"),"OctupleCompressedGravel");
 		reg(OctupleCompressedSand = new OctupleCompressedSand("OctupleCompressedSand"),"OctupleCompressedSand");
 		reg(QuadrupleCompressedCoalBlock = new QuadrupleCompressedCoalBlock("QuadrupleCompressedCoalBlock"),"QuadrupleCompressedCoalBlock");
 		reg(QuadrupleCompressedCobblestone = new QuadrupleCompressedCobblestone("QuadrupleCompressedCobblestone"),"QuadrupleCompressedCobblestone");
 		reg(QuadrupleCompressedDirt = new QuadrupleCompressedDirt("QuadrupleCompressedDirt"),"QuadrupleCompressedDirt");
-		reg(QuadrupleCompressedFlint = new QuadrupleCompressedFlint("QuadrupleCompressedFlint"),"QuadrupleCompressedFlint");
 		reg(QuadrupleCompressedGravel = new QuadrupleCompressedGravel("QuadrupleCompressedGravel"),"QuadrupleCompressedGravel");
 		reg(QuadrupleCompressedSand = new QuadrupleCompressedSand("QuadrupleCompressedSand"),"QuadrupleCompressedSand");
 		reg(QuintupleCompressedCobblestone = new QuintupleCompressedCobblestone("QuintupleCompressedCobblestone"),"QuintupleCompressedCobblestone");
 		reg(QuintupleCompressedDirt = new QuintupleCompressedDirt("QuintupleCompressedDirt"),"QuintupleCompressedDirt");
-		reg(QuintupleCompressedFlint = new QuintupleCompressedFlint("QuintupleCompressedFlint"),"QuintupleCompressedFlint");
 		reg(QuintupleCompressedGravel = new QuintupleCompressedGravel("QuintupleCompressedGravel"),"QuintupleCompressedGravel");
 		reg(QuintupleCompressedSand = new QuintupleCompressedSand("QuintupleCompressedSand"),"QuintupleCompressedSand");
 		reg(SeptupleCompressedCobblestone = new SeptupleCompressedCobblestone("SeptupleCompressedCobblestone"),"SeptupleCompressedCobblestone");
 		reg(SeptupleCompressedDirt = new SeptupleCompressedDirt("SeptupleCompressedDirt"),"SeptupleCompressedDirt");
-		reg(SeptupleCompressedFlint = new SeptupleCompressedFlint("SeptupleCompressedFlint"),"SeptupleCompressedFlint");
 		reg(SeptupleCompressedGravel = new SeptupleCompressedGravel("SeptupleCompressedGravel"),"SeptupleCompressedGravel");
 		reg(SeptupleCompressedSand = new SeptupleCompressedSand("SeptupleCompressedSand"),"SeptupleCompressedSand");
 		reg(SextupleCompressedCobblestone = new SextupleCompressedCobblestone("SextupleCompressedCobblestone"),"SextupleCompressedCobblestone");
 		reg(SextupleCompressedDirt = new SextupleCompressedDirt("SextupleCompressedDirt"),"SextupleCompressedDirt");
-		reg(SextupleCompressedFlint = new SextupleCompressedFlint("SextupleCompressedFlint"),"SextupleCompressedFlint");
 		reg(SextupleCompressedGravel = new SextupleCompressedGravel("SextupleCompressedGravel"),"SextupleCompressedGravel");
 		reg(SextupleCompressedSand = new SextupleCompressedSand("SextupleCompressedSand"),"SextupleCompressedSand");
 		reg(TripleCompressedCoalBlock = new TripleCompressedCoalBlock("TripleCompressedCoalBlock"),"TripleCompressedCoalBlock");
 		reg(TripleCompressedCobblestone = new TripleCompressedCobblestone("TripleCompressedCobblestone"),"TripleCompressedCobblestone");
 		reg(TripleCompressedDirt = new TripleCompressedDirt("TripleCompressedDirt"),"TripleCompressedDirt");
-		reg(TripleCompressedFlint = new TripleCompressedFlint("TripleCompressedFlint"),"TripleCompressedFlint");
 		reg(TripleCompressedGravel = new TripleCompressedGravel("TripleCompressedGravel"),"TripleCompressedGravel");
 		reg(TripleCompressedSand = new TripleCompressedSand("TripleCompressedSand"),"TripleCompressedSand");
-		
+
 		regFuel();
-	    
+
 	}
 }
--- src/main/java/com/kreezxil/compressedblocks/blocks/CompressedFlint.java
+++ src/main/java/com/kreezxil/compressedblocks/blocks/CompressedFlint.java
@@ -2,43 +2,31 @@ package com.kreezxil.compressedblocks.blocks;
 
 import java.util.Random;
 
+import com.kreezxil.compressedblocks.CompressedBlocks;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
-import com.kreezxil.compressedblocks.CompressedBlocks;
-
 public class CompressedFlint extends Block {
-	
 
-
-	public CompressedFlint(String unlocalizedName, Material materialIn, float hardness, float resistance) {
-		super(materialIn);
-		// TODO Auto-generated constructor stub
-		this.setUnlocalizedName(unlocalizedName);
+	public CompressedFlint() {
+		super(Material.rock);
+		this.setUnlocalizedName("CompressedFlint");
 		this.setCreativeTab(CompressedBlocks.blocksTab);
-		this.setHardness(hardness);
-		this.setResistance(resistance);
+		this.setHardness(10f);
+		this.setResistance(2f);
 		this.setHarvestLevel("pickaxe",0);
 	}
-	
-	public CompressedFlint(String name, float hardness, float resistance) {
-		this(name, Material.rock, hardness, resistance);
-	}
-	
-	public CompressedFlint(String name) {
-		this(name,10f, 2f);
-	}
 
+	@Override
 	public int quantityDropped(Random par1Random){
 		return 9;
-		}
+	}
+	@Override
 	public Item getItemDropped(IBlockState state, Random par2Random, int par3){
 		return Items.flint;
 	}
-
-
 }
