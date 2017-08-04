e14c5147d1b661063df40f7f9a2e1fb17f8b89ef
Kreezxil <kreezxil@gmail.com>
2017-08-04 09:14:05 -0500

    1.12.1
    grrrr

--- src/main/java/com/kreezxil/compressedblocks/CompressedBlocks.java
+++ src/main/java/com/kreezxil/compressedblocks/CompressedBlocks.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CompressedBlocks {
 	public static final String MODNAME = "Compressed Blocks by Kreezxil";
 	public static final String MODID = "kreezxilscompressedblocks";
-	public static final String VERSION = "1.12.1-4.1.0.1578";
+	public static final String VERSION = "1.12.1-4.1.0.1578";//
 
 	public static CreativeTabs itemsTab = new ItemsTab("itemsTab");
 	public static CreativeTabs blocksTab = new BlocksTab("blocksTab");
